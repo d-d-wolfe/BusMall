50cc6c849aea995e613736e956e1b929e7554d9f
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-14 19:14:43 -0700

    Initial styling of the chart.js

--- app.js
+++ app.js
@@ -168,8 +168,21 @@ var chart = new Chart(ctx, {
         labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
         datasets: [{
             label: 'Bus Mall Items',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: ['rgb(104, 99, 99)',
+            'rgba(1, 1, 1, 0.4)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
